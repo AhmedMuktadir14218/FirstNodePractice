800d6fe04aa5cbe840efdb5d465fc758c4a6618e
DESKTOP-ENNB4QI\udoym <ahmed15-14218@diu.edu.bd>
2022-08-08 13:52:20 +0600

    first node practice done

---
+++ _setup.js
@@ -0,0 +1,17 @@
+/**
+ * 1. create a foldar (manually/mkdir)
+ * 2. open comd line or folder
+ * 3.npm init
+ * 4.npm install exxpress
+ * 5.create a folder index.js
+ * .....................
+ * 
+ * 1, require expree
+ * create app variable
+ * declare port
+ * set app.get('/)
+ * lixten to port
+ * node,index.js
+ * check your for that port
+ * 
+ */
